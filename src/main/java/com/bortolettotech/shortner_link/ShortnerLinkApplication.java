fa2f2d1531a632497c6d7ff7c0f76a1859d790da
João G. Bortoletto <jgb3532@hotmail.com>
2025-07-13 15:22:10 -0300

    chore: project starter

---
+++ src/main/java/com/bortolettotech/shortner_link/ShortnerLinkApplication.java
@@ -0,0 +1,13 @@
+package com.bortolettotech.shortner_link;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortnerLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortnerLinkApplication.class, args);
+	}
+
+}
